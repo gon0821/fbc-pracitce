2da7eff9d311fd4b8cad380795003012a0d00a0b
gon821 <gonty821@gmail.com>
2026-03-15 15:21:05 +0900

    test-unit使ってみる

--- 01.test/leap.rb
+++ 01.test/leap.rb
@@ -3,16 +3,18 @@
 # 2.ただし、西暦年が100で割り切れる年は(原則として)平年。
 # 3.ただし、西暦年が400で割り切れる年は必ず閏年。
 
-def leap?(year)
-  if year % 400 == 0
-    true
-  elsif year % 100 == 0
-    false
-  elsif year % 4 == 0
-    true
-  else
-    false
+class Sample
+  def self.leap?(year)
+    if year % 400 == 0
+      true
+    elsif year % 100 == 0
+      false
+    elsif year % 4 == 0
+      true
+    else
+      false
+    end
   end
 end
 
-puts leap?(ARGV[0].to_i)
+# puts Sample.leap?(ARGV[0].to_i)
